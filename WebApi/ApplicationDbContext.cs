7a4f0e06b94ff12de2dc2f679a898b1a415d5b00
yordy <yordysh28@gmail.com>
2023-02-02 17:29:56 -0500

    Add entity Cine

---
+++ WebApi/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApi
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Genero>().HasKey(prop => prop.Id);
+            modelBuilder.Entity<Genero>().Property(prop => prop.Nombre)
+                .HasMaxLength(150).IsRequired();
+
+            modelBuilder.Entity<Actor>().Property(prop => prop.Nombre)
+                .HasMaxLength(150).IsRequired();
+            modelBuilder.Entity<Actor>().Property(prop => prop.FechaNacimiento)
+                .HasColumnType("date");
+        }
+        public DbSet<Genero> Generos { get; set; }
+        public DbSet<Actor> Actores { get; set; }
+
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using WebApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+var connectionString = builder.Configuration.GetConnectionString("DefaulConnection");
+IServiceCollection serviceCollection = builder.Services.AddDbContext<ApplicationDbContext>(opciones => opciones.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
